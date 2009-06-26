08d27b3833811cc16c5b4b1432bc6b32580a7b14
Jean Azzopardi <lotrlordillidan@gmail.com>
2009-06-27 01:28:34 +0200

    Edited sprite sheet program

--- sprite_sheet.cpp
+++ sprite_sheet.cpp
@@ -9,11 +9,13 @@ const int SCREEN_HEIGHT = 480;
 const int SCREEN_BPP	= 32;
 
 //The surfaces
-SDL_Surface *image 	= NULL;
+SDL_Surface *dots 	= NULL;
 SDL_Surface *screen 	= NULL;
 
 SDL_Event event;
 
+//The portions of the sprite map to be blitted.
+SDL_Rect clip[4];
 SDL_Surface *load_image( std::string filename ) 
 { 
 	 
@@ -30,12 +32,14 @@ SDL_Surface *load_image( std::string filename )
 	return optimizedImage; 
 } 
 
-void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination ) 
+void apply_surface( int x, int y, SDL_Surface* source, SDL_Surface* destination,SDL_Rect* clip = NULL ) 
 { 
 	SDL_Rect offset; 
+
 	offset.x = x; 
 	offset.y = y; 
-	SDL_BlitSurface( source, NULL, destination, &offset ); 
+
+	SDL_BlitSurface( source, clip, destination, &offset ); 
 }
 
 bool init()
@@ -57,24 +61,16 @@ bool init()
 
 bool load_files()
 {
-	image = load_image("background.png");
-	SDL_Surface* second_image = NULL;
-	second_image = load_image("foo.png");
-	
-	if (second_image != NULL)
-	{
-		Uint32 colorkey = SDL_MapRGB(second_image->format,0,0xFF,0xFF);
-		SDL_SetColorKey(second_image,SDL_SRCCOLORKEY,colorkey);
-	}
-	apply_surface(240,190,second_image,image);
-
-	if (image == NULL) return false;
+	dots = load_image("dots.png");
+	Uint32 colorkey = SDL_MapRGB(dots->format,0,0xFF,0xFF);
+	SDL_SetColorKey(dots,SDL_SRCCOLORKEY,colorkey);
+	if (dots == NULL) return false;
 	return true;
 }
 
 void clean_up()
 {
-	SDL_FreeSurface(image);
+	SDL_FreeSurface(dots);
 	SDL_Quit();
 }
 
@@ -85,7 +81,39 @@ int main(int argc, char* args[])
 	
 	if (load_files() == false) return 1;
 	
-	apply_surface(0,0,image,screen);
+	//Top left
+	clip[0].x	= 0;
+	clip[0].y	= 0;
+	clip[0].w	= 100;
+	clip[0].h	= 100;
+	
+	//Top right
+	clip[1].x	= 100;
+	clip[1].y	= 0;
+	clip[1].w	= 100;
+	clip[1].h	= 100;
+	
+	//bottom left
+	clip[2].x	= 0;
+	clip[2].y	= 100;
+	clip[2].w	= 100;
+	clip[2].h	= 100;
+	
+	//bottom right
+	clip[3].x 	= 100;
+	clip[3].y	= 100;
+	clip[3].w	= 100;
+	clip[3].h	= 100;
+	
+	//Fill the screen Purple
+	SDL_FillRect(screen,&screen->clip_rect,SDL_MapRGB(screen->format,0xFF,0x00,0xFF));
+	
+	//Apply images to the screen.
+	apply_surface(0,0,dots,screen,&clip[0]);
+	apply_surface(540,0,dots,screen,&clip[1]);
+	apply_surface(0,380,dots,screen,&clip[2]);
+	apply_surface(540,380,dots,screen,&clip[3]);	
+	
 	
 	if (SDL_Flip(screen) == -1) return 1;
 	
